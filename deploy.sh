b65f49f874721b1dcfb94565894a2f469b3cd391
Jonah Dlin <jonah.dlin@gmail.com>
2022-11-27 22:06:27 -0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 
-folder=$1
+git checkout main
+git fetch
+git pull
 
-cd folder
-rm -rf poker
-git clone https://github.com/jonahdlin/poker.git
-
-cd poker/server
+cd server
 npm run build-prod
 
-cd ../../
-mv poker/server/dist ./
-
 pm2 delete server
-pm2 start server.js
+pm2 start dist/server.js
